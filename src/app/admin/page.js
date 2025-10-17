47aadce7146945034e49b0d29e2ff6b478d3f02b
Harry Newman <h.newman2005@outlook.com>
2025-10-17 21:31:02 +0100

    Add Supabase client, admin page, and update all page structure

---
+++ src/app/admin/page.js
@@ -0,0 +1,9 @@
+export default function Admin() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen font-mono text-3xl">
+            <div>
+                <p>tinyrev admin</p>
+            </div>
+        </div>
+    )
+}
