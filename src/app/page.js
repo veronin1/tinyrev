41731b65cd3c027838b488e4bea893d0f1d20ea7
Harry Newman <h.newman2005@outlook.com>
2025-10-22 19:35:17 +0100

    Added underline on hover

--- src/app/page.js
+++ src/app/page.js
@@ -12,7 +12,7 @@ export default function Home() {
 
             <div className="flex flex-col items-center justify-center min-h-screen font-mono text-3xl">
                 <div>
-                    <Link href="/">tinyrev</Link>
+                    <Link href="/" className="hover:underline">tinyrev</Link>
                 </div>
 
                 <div className="search mt-4">
@@ -26,16 +26,16 @@ export default function Home() {
                 <div className="footer mt-8">
                     <ul className="flex gap-4">
                         <li>
-                            <Link href="/all">all</Link>
+                            <Link href="/all" className="hover:underline">all</Link>
                         </li>
                         <li>
-                            <Link href="/movies">movies</Link>
+                            <Link href="/movies" className="hover:underline">movies</Link>
                         </li>
                         <li>
-                            <Link href="/television">television</Link>
+                            <Link href="/television" className="hover:underline">television</Link>
                         </li>
                         <li>
-                            <Link href="/games">games</Link>
+                            <Link href="/games" className="hover:underline">games</Link>
                         </li>
                     </ul>
                 </div>
