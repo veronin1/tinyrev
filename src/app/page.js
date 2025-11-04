a1a25250deeb5a5ec239ac7f20bf1255a82d0aef
Harry Newman <h.newman2005@outlook.com>
2025-11-04 18:24:11 +0000

    Created simple function to call searchDatabase.js on searchBar

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,19 @@
+'use client'
+
+import {useState} from "react";
 import Link from "next/link";
+import {searchDatabase} from "../../utils/supabase/searchDatabase";
 
 export default function Home() {
+    const [query, setQuery] = useState("");
+
+    async function handleSearch(e) {
+        const q = e.target.value;
+        setQuery(q);
+
+        const searchResults = await searchDatabase(query);
+    }
+
     return (
         <main className="flex flex-col items-center justify-center flex-1 px-6 py-20 text-center">
             <Link
@@ -18,6 +31,9 @@ export default function Home() {
                 type="search"
                 placeholder="search..."
                 className="mt-8 border border-neutral-300 bg-white px-4 py-2 w-72 rounded-md text-center focus:outline-none focus:ring-2 focus:ring-[var(--accent)] transition-all"
+                id="search"
+                value={query}
+                onChange={handleSearch}
             />
 
             <ul className="flex gap-8 mt-10 text-sm uppercase text-neutral-600">
